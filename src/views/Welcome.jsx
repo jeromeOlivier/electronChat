45796c2b584dd81b3973c3dda8abf3004c46e482
jeromeOlivier <jerome.olivier@icloud.com>
2024-07-01 17:26:57 -0400

    toggle register / login forms

--- src/views/Welcome.jsx
+++ src/views/Welcome.jsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { LoginForm, RegisterForm } from '../components/';
 
 export default function Welcome() {
+  const [isLoginIn, setIsLogIn] = useState(true);
+
+  const toggleForm = () => setIsLogIn(!isLoginIn);
+  const form = isLoginIn ? <LoginForm /> : <RegisterForm />;
+  const message = isLoginIn ? `Don't have an account?` : `Already registered?`;
+  const action = isLoginIn ? `Register` : `Login`;
+
   return (
     <div className="centered-view">
       <div className="centered-container">
-        <LoginForm />
+        {form}
         <small className="form-text text-muted mt-2">
-          Already registered?
-          <span onClick={() => {}} className="btn-link ml-2">
-            Login
+          {message}
+          <span onClick={toggleForm} className="btn-link ml-2">
+            {action}
           </span>
         </small>
       </div>
