9a2b9b13b50e702811723b95f32127dc739827ef
jeromeOlivier <jerome.olivier@icloud.com>
2024-07-03 07:55:18 -0400

    user profile

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,9 +1,22 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import db from '../db/firestore';
 
-export async function register({ email, password}) {
+export const createUserProfile = (userProfile) =>
+  db.collection('profiles').doc(userProfile.uid).set(userProfile);
+
+export async function register({ email, password, username, avatar }) {
   try {
-    return await firebase.auth().createUserWithEmailAndPassword(email, password);
+    const { user } = await firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password);
+    await createUserProfile({
+      uid: user.uid,
+      username,
+      email,
+      avatar,
+      joinedChats: [],
+    });
   } catch (error) {
     return Promise.reject(error.message);
   }
